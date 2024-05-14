0c84796a172e128f643dcb21154e5885d8100cab
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2024-05-14 19:01:56 -0300

    substituindo ckass por id

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+let nomeUsuario = document.getElementById('nome');
+let senhaUsuario = document.getElementById('senha');
+let salvarDados = document.getElementById('salvar-dados');
+let enviar = document.getElementsById('btn'); 
+
+
+
+
+console.log('senha')
+console.log('nome')
+console.log('btn')
+console.log('salvar-dados')
+
