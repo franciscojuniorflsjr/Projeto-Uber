1602359adc364f6143a046db1e05aa5e00b13e31
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2024-05-16 10:33:52 -0300

    adicionando tag span e colocando erro caso identifique campos vazios no formulário.

--- js/main.js
+++ js/main.js
@@ -15,16 +15,19 @@ e.preventDefault();
 })
 
 */
-function validaEntrada(){
-    const emailValidoConfirmado = emailValido(); 
-    document.getElementById('reset-senha').disabled = 
-    !emailValidoConfirmado;
-    
-    const senhaConfirmada = validarSenha();
-    document.getElementById('btn').disabled = !emailValidoConfirmado ||
-    !senhaConfirmada;
+    function validarEntradaEmail(){
+        alterarBotaoDesabilitado();
+        alterarErroEmail();
+      
 
-}   
+}  
+    function ValidarEntradaSenha(){
+        alterarBotaoDesabilitado();
+        alertaErroSenha();
+        
+    }
+
+
 
     function emailValido() {
         const email = document.getElementById('email').value; 
@@ -34,6 +37,47 @@ function validaEntrada(){
             return validateEmail(email);
 }
    
+    function alterarErroEmail(){
+        const email = document.getElementById('email').value;
+        if(!email) {
+            document.getElementById("email-obrigatorio-erro")
+            .style.display = "block";
+        } else {
+            document.getElementById("email-obrigatorio-erro")
+            .style.display = "none";
+        }
+
+        if(validateEmail(email)){
+            document.getElementById('email-invalido-erro')
+            .style.display="none";
+        } else {
+            document.getElementById('email-invalido-erro')
+            .style.display="block";
+        }
+
+    }
+
+    function alertaErroSenha(){
+        const senha = document.getElementById('senha').value;
+        if(!senha){
+            document.getElementById('senha-obrigatoria-erro')
+            .style.display="block";
+        } else {
+            document.getElementById('senha-obrigatoria-erro')
+            .style.display="none";
+        }
+    }
+
+    function alterarBotaoDesabilitado(){
+        const emailValidoConfirmado = emailValido(); 
+        document.getElementById('reset-senha').disabled = 
+        !emailValidoConfirmado;
+        
+        const senhaConfirmada = validarSenha();
+        document.getElementById('btn').disabled = !emailValidoConfirmado ||
+        !senhaConfirmada;
+    }
+
     function validarSenha(){
       const senha = document.getElementById('senha').value;
       if (!senha){
